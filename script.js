259095288149ad97d7421ddbb7cd4f9f2d9ec07f
Carlos Reges <kerg23@gmail.com>
2022-06-17 20:29:30 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let nombre = prompt ("Hola, internauta. ¿Cómo te llamas?");
+
+let edad = prompt ("Veo que quieres manejar un automovil. Antes te pregunto: ¿Qué edad tienes?");
+
+function puedoConducir(nombre, edad) {
+    if (edad < 18){
+        console.log(`Lo siento, ${nombre}. No puedes conducir aún.`);
+    } else {
+        console.log(`${nombre}, por supuesto que puedes conducir`);
+    }
+}
+
+puedoConducir(nombre, edad);
