42324828ce61d07f580bd0b650344bb52323da3a
Carlos Reges <kerg23@gmail.com>
2022-06-17 21:12:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ let edad = prompt ("Veo que quieres manejar un automovil. Antes te pregunto: ¿Q
 function puedoConducir(nombre, edad) {
     if (edad < 18){
         console.log(`Lo siento, ${nombre}. No puedes conducir aún.`);
+        document.write(`Lo siento, ${nombre}. No puedes conducir aún.`);
     } else {
         console.log(`${nombre}, por supuesto que puedes conducir`);
+        document.write(`${nombre}, por supuesto que puedes conducir`);
     }
 }
 
